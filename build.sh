ec39930563ef29b6a5ce2cf0d2d4425a96e4ef63
hafiz-udchalo <61221372+hafiz-udchalo@users.noreply.github.com>
2020-06-04 17:56:18 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ export BUCKET=uc-api-gateway-$NODE_ENV
 sed -i=' ' "s/<NODE_ENV>/$NODE_ENV/g" swagger.yaml
 sed -i=' ' "s/<DOMAIN_URL>/$DOMAIN_URL/g" swagger.yaml
 sed -i=' ' "s/<USER_URL>/$USER_URL_URL/g" swagger.yaml
-sed -i=' ' "s/<STACK>/$STACK/g" template.yaml
+#sed -i=' ' "s/<STACK>/$STACK/g" template.yaml
 sed -i=' ' "s/<NODE_ENV>/$NODE_ENV/g" template.yaml
 
 aws s3 mb s3://$BUCKET
