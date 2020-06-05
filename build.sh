16ae55e68abe1eb2be40619eee8707d702e32edd
hafiz-udchalo <61221372+hafiz-udchalo@users.noreply.github.com>
2020-06-05 08:18:40 +0530

    add sam command

--- build.sh
+++ build.sh
@@ -18,10 +18,10 @@ sed -i='' "s/<NODE_ENV>/$NODE_ENV/g" template.yaml
 #aws s3 mb s3://$BUCKET
 
 # Uploads files to S3 bucket and creates CloudFormation template
-#sam package --template-file template.yaml --s3-bucket $BUCKET --output-template-file package.yaml
+sam package --template-file template.yaml --s3-bucket $BUCKET --output-template-file package.yaml
 
 # Deploys your stack   
-#sam deploy --template-file package.yaml --stack-name $STACK --capabilities CAPABILITY_IAM
+sam deploy --template-file package.yaml --stack-name $STACK --capabilities CAPABILITY_IAM
 
 
 
