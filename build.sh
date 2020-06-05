25f22dcafc0ea17771b8a7f5459e0b14815fa366
hafiz-udchalo <61221372+hafiz-udchalo@users.noreply.github.com>
2020-06-05 08:59:11 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,10 +5,12 @@ set -x
 #export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 
 export AWS_DEFAULT_REGION=ap-south-1
-export STACK=$NODE_ENV-api-gateway
+export STACK=$1-api-gateway
 export BUCKET=uc-deploy-api-gateway-$1
 
 echo "$DOMAIN_URL $USER_URL $1"
+DOMAIN_URL="https://$1-server.udchalo.com"
+USER_URL="https://users-$1-api.udchalo.com"
 
 sed -i='' "s@<DOMAIN_URL>@$DOMAIN_URL@g" swagger.yaml
 sed -i='' "s@<USER_URL>@$USER_URL@g" swagger.yaml
