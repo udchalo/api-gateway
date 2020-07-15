5ebc7edff614b72ba09225c7f00aea8464132615
hafiz-udchalo <61221372+hafiz-udchalo@users.noreply.github.com>
2020-07-15 13:20:19 +0530

    updated sam command

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ sed -i='' "s/<NODE_ENV>/$1/g" template.yaml
 sam package --template-file template.yaml --s3-bucket $BUCKET --output-template-file package.yaml
 
 # Deploys your stack   
-sam deploy --template-file package.yaml --stack-name $STACK --capabilities CAPABILITY_IAM
+sam deploy --template-file package.yaml --stack-name $STACK --capabilities CAPABILITY_IAM --no-fail-on-empty-changeset
 
 
 
