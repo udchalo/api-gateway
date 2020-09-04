96abdebf75b8451272f50bd263b97e8c0cb9deba
hafiz-udchalo <61221372+hafiz-udchalo@users.noreply.github.com>
2020-09-04 13:16:20 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,13 +10,13 @@ export BUCKET=uc-deploy-api-gateway-$1
 
 echo "$DOMAIN_URL $USER_URL $1"
 
-DOMAIN_URL="http://ucbeanstalkapplications-stage.eba-emtxm8qt.ap-south-1.elasticbeanstalk.com:8080"
-ORIGIN_URL="https://$1-ui.udchalo.com"
+DOMAIN_URL="http://ucbeanstalkapplications-stage.eba-emtxm8qt.ap-south-1.elasticbeanstalk.com"
+#ORIGIN_URL="https://$1-ui.udchalo.com"
 echo $ORIGIN_URL
 
 sed -i='' "s@<DOMAIN_URL>@$DOMAIN_URL@g" swagger.yaml
 sed -i='' "s@<USER_URL>@$USER_URL@g" swagger.yaml
-sed -i -e "s@<ORIGIN_URL>@$ORIGIN_URL@g" swagger.yaml
+#sed -i -e "s@<ORIGIN_URL>@$ORIGIN_URL@g" swagger.yaml
 sed -i='' "s/<NODE_ENV>/$1/g" swagger.yaml
 sed -i='' "s/<NODE_ENV>/$1/g" template.yaml
 
